cfd7e528454ce587f9e653b590b860b1e1b9238d
S A U R A B H  W A J E <saurabhwaje4770@gmail.com>
2024-02-01 21:06:36 +0530

    Created Cloudinary service

---
+++ src/utils/cloudinary.service.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    // UPLOAD FILE ON CLOUD
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+      format: "auto",
+    });
+
+    console.log(">> ", response.url);
+    console.log(">> ", response);
+    console.log("File Successfully Uploaded!");
+
+    return response;
+  } catch (error) {
+    // Remove locally save temporary file upon the failed operation
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
+
+export { uploadCloudinary };
