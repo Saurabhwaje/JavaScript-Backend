26c48c8c3a9d86d2f1f1f406e3ac0d14c8ba5625
S A U R A B H  W A J E <saurabhwaje4770@gmail.com>
2024-03-13 14:05:49 +0530

    created Login and Logout (functions and middleware)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,25 @@ import { uploadCloudinary } from "../utils/cloudinary.service.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 // import {abc} from "../../public/temp"
 
+// generate access and refresh tokens
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong! While generating access token"
+    );
+  }
+};
+
 // Route handler for registering a new user
 const registerUser = asyncHandler(async (req, res) => {
   // Destructure fields from request body
@@ -66,9 +85,9 @@ const registerUser = asyncHandler(async (req, res) => {
   console.log("User Object: ", user);
 
   // Retrieve the created user details from the database
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
+  const createdUser = await user
+    .findById(user._id)
+    .select("-password -refreshToken");
 
   // Throw an error if user creation fails
   if (!createdUser) {
@@ -81,8 +100,70 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully!"));
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  if (!email || !username) {
+    throw new ApiError(400, "Username or Password is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isMatch = await user.isPasswordCorrect(password);
+  if (!isMatch) {
+    throw new ApiError(401, "Invalid password");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = { httpOnly: true, secure: true };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  console.log(req.user);
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = { httpOnly: true, secure: true };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out"));
+});
+
 // Export the registerUser route handler
-export { registerUser };
+export { registerUser, loginUser, logoutUser };
 
 // get user details -> validations -> user is existing or not( based on email / username ) -> chaek for images, avatar
 // Upload item cloudinary, avatar ->
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -77,7 +77,7 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
-userSchema.methods.generateRefreshToken = function () {
+0 .methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
