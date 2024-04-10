0deff999d71780228d460d143e8c9592548707c3
S A U R A B H  W A J E <saurabhwaje4770@gmail.com>
2024-04-10 23:36:40 +0530

    user controller Pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -318,13 +318,57 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       $match: { username: username?.toLowerCase() },
     },
     {
-      $lookup: { from: "subscriptions" },
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        subscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+        email: 1,
+        avatar: 1,
+        coverimage: 1,
+        createdAt: 1,
+        updatedAt: 1,
+      },
     },
   ]);
 
-  if (userChannel) {
+  if (!userChannel.length) {
     throw new ApiError(400, "User Channel not found");
   }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userChannel[0], "userChannel fetched"));
 });
 
 // Export the registerUser route handler
